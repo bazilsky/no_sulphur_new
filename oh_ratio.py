12f9fb30fabd887b46f2c3ba3b75c14fd08f2451
ANANTH RANJITHKUMAR <eeara@sci2.jasmin.ac.uk>
2021-01-26 10:16:54 +0000

    sulphurless+biogenic a2.py

--- oh_ratio.py
+++ oh_ratio.py
@@ -33,14 +33,6 @@ mpath1='/group_workspaces/jasmin2/asci/eeara/model_runs/u-ca206/2017jan/' # sulp
 #mpath2='/group_workspaces/jasmin2/asci/eeara/model_runs/u-ca179/2017jan/' # new basline sulphurless planet
 mpath2='/group_workspaces/jasmin2/asci/eeara/model_runs/u-ca179/2017jan/' # sulphurless planet
 
-mpath1='/gws/nopw/j04/asci/eeara/model_runs/u-ca954/2017jan/' # present day with biogenic nucleation ON
-#mpath2='/group_workspaces/jasmin2/asci/eeara/model_runs/u-ca179/2017jan/' # new basline sulphurless planet
-mpath2='/gws/nopw/j04/asci/eeara/model_runs/u-ca439/2017jan/' # present day planet
-
-mpath1='/gws/nopw/j04/asci/eeara/model_runs/u-cb602/All_months/' # present day with biogenic nucleation ON
-#mpath2='/group_workspaces/jasmin2/asci/eeara/model_runs/u-ca179/2017jan/' # new basline sulphurless planet
-mpath2='/gws/nopw/j04/asci/eeara/model_runs/u-ca440/All_months/' # present day planet
-
 alt_path='/group_workspaces/jasmin2/asci/eeara/model_runs/u-bf829/L1/'
 #alt_path='/gws/nopw/j04/gassp/hgordon/u-be424-noColinFix/L1/'
 #mpath2='/group_workspaces/jasmin2/gassp/eeara/model_runs/u-bc244/L1/'
@@ -132,7 +124,7 @@ def get_model_data(indx,alt,num,mpath): #add variable for cutoff and do logcumto
     #print model_n
     model_n = model_n.collapsed('time',iris.analysis.MEAN)
     model_n = model_n/1000000
-    print(model_n)
+    #print(model_n)
     #model
     model_n.long_name='Particle Number Concentration at STP (cm-3)'
     model_n.units='cm-3'
@@ -168,37 +160,82 @@ for i in flag:
     trop_height = trop_height.collapsed('longitude',iris.analysis.MEAN)
     trop_height = trop_height/1000.0
     
-    cn_file = 'All_months_m01s38i437_CN__NUMBER_CONCENTRATION____________.nc'
-    #cn_file = '2017jan_m01s38i437_CN__NUMBER_CONCENTRATION____________.nc'
-    
+    #cn_file = 'All_months_m01s38i437_CN__NUMBER_CONCENTRATION____________.nc'
+    cn_file = '2017jan_m01s38i437_CN__NUMBER_CONCENTRATION____________.nc'
     #if i==0:
     if i%3==0:
         #new1=get_model_data(i,alt_data[i],i,mpath1)
         #new2=get_model_data(i,alt_data[i],i,mpath2)
          
-        new1 = iris.load(mpath1+cn_file)[0]
-        new2 = iris.load(mpath2+cn_file)[0]
-       
-        new1 = new1.collapsed('time',iris.analysis.MEAN)
-        #new1 = new1[5,:,:,:]
+        #new1 = iris.load(mpath1+cn_file)[0]
+        #new2 = iris.load(mpath2+cn_file)[0]
+        
+        secorg_path='/gws/nopw/j04/asci/eeara/model_runs/u-cb395/'
+        secorg_path2='/gws/nopw/j04/asci/eeara/model_runs/u-cb448/2017jan/' # only secorg monthly output
+        
+        secorgfromoho3_393 = 'secorgfromoho3_393.nc'
+        secorgfromoho3_395 = 'secorgfromoho3_395.nc'
+        sec_org_dummy = 'sec_org_dummy.nc'
+        
+        sec_org_dummy_2 = '2017jan_m01s38i439_CCN_NO._CONCENTRN._(ACC+COR+AIT>25r).nc'
+        secorgfromoho3_2 = '2017jan_m01s38i440_CCN_NO._CONCENTRN._(ACC+COR+AIT>35r).nc' 
+
+        new1 = iris.load(secorg_path+secorgfromoho3_393)[0]
+        new2 = iris.load(secorg_path+secorgfromoho3_395)[0]
+        new3 = iris.load(secorg_path+sec_org_dummy)[0]
+        
+        new3_dummy = iris.load(secorg_path2+sec_org_dummy_2)[0]
+        new4 = iris.load(secorg_path2+secorgfromoho3_2)[0]
+        
+        
+        o3_oh_path = '/gws/nopw/j04/asci/eeara/model_runs/u-cb393/'
+
+        o3_file = 'o3.nc'
+        oh_file = 'oh.nc'
+        o3_file_new = 'o3_calcnucrate.nc'
+        oh_file_new = 'oh_calcnucrate.nc'
+        
+
+        o3 = iris.load(o3_oh_path+o3_file)[0]
+        oh = iris.load(o3_oh_path+oh_file)[0]
+        oh_new = iris.load(o3_oh_path+oh_file_new)[0]
+        o3_new = iris.load(o3_oh_path+o3_file_new)[0]
+
+
+        #diff_dummy = new2 - new1
+        #diff_dummy = new2/new1
+        #diff_dummy = new3-new1
+        #diff_dummy = new4-new3_dummy
+        #diff_dummy = new3_dummy-new4
+        #diff_dummy = new3_dummy/new4
+        #diff_dummy = new4 
+        #diff_dummy_2 = o3_new.data[2,:,:,:]/o3.data[2,:,:,:]
+        diff_dummy_2 = oh_new.data[2,:,:,:] / oh.data[2,:,:,:]
+        
+       # diff_dummy_2 = oh_new.data[2,:,:,:] - oh.data[2,:,:,:]
+        
+        dummy_cube = new1.copy()[0,:,:,:]
+        dummy_cube.data = diff_dummy_2
+        diff_dummy = dummy_cube.copy()
+        diff_dummy = diff_dummy.collapsed('longitude',iris.analysis.MEAN)
+        #diff_dummy = diff_dummy.collapsed('time',iris.analysis.MEAN)
+
+        
         new1 = new1.collapsed('longitude',iris.analysis.MEAN)
-        #new2 = new2[5,:,:,:]
-        new2 = new2.collapsed('time',iris.analysis.MEAN)
+        new1 = new1.collapsed('time',iris.analysis.MEAN)
         new2 = new2.collapsed('longitude',iris.analysis.MEAN)
+        new2 = new2.collapsed('time',iris.analysis.MEAN)
         
          
 
-        print('this is longitudenal mean\n\n', new1,'\n\n')
-        new1=new1[0:66,:]
-        new2=new2[0:66,:]
- #       old1=get_model_data(i,alt_data[i],i,mpath2)
- #       diff=((new1-old1)/old1)*100
-      #  data, lat, lon = file_save(new1,i,alt_data[i])
+        #print('this is longitudenal mean\n\n', new1,'\n\n')
+        #new1=new1[0:66,:]
+        #new2=new2[0:66,:]
         
         #qplt.contourf(new1)
         #plt.show()
-        diff = new1.data-new2.data # biogenic - baseline simulation
-         
+        #diff = new2.data-new1.data # biogenic - baseline simulation
+        diff = diff_dummy.data[0:52,:] 
         #data=new1.data
         data = diff.data
         lnum1=new1.coord('model_level_number').points
@@ -208,7 +245,8 @@ for i in flag:
                     
            
             
-        lnum_alt=alt_data[0:66]
+        lnum_alt=alt_data[0:52]
+        #lnum_alt=alt_data[0:66]
         lat1=new1.coord('latitude').points
         x,y=np.meshgrid(lat1,lnum_alt)
         #plt.figure()
@@ -230,14 +268,20 @@ for i in flag:
         
        # ticks=[1e0,5e0,1e1,2e1,5e1,1e2,2e2,5e2,1e3,2e3,5e3,1e4,3e4,6e4,1e5]
         ticks4 = [-1e3,-1e2,-1e1,1e1,1e2,1e3] 
-        ticks4 = [-5e3,-1e3,-5e2,-1e2,-5e1,-1e1,1e1,5e1,1e2,5e2,1e3,5e3] 
+        ticks4 = [-1e4,-5e3,-1e3,-5e2,-1e2,-5e1,-1e1,1e1,5e1,1e2,5e2,1e3,5e3,1e4] 
+        ticks4 = [-1e7,-1e6,-1e5,-1e4,-1e3,-1e2,-1e1,1e1,1e2,1e3,1e4,1e5,1e6,1e7] 
+        #ticks4 = [-1e200,-1e100,-1e50,-1e30,-1e10,-1e1,1e1,1e10,1e30,1e50,1e100,1e200] 
+        ticks4 = np.arange(0,1.1,0.1)
+        ticks4 = [0.1,0.4,0.8,1e0,1e1,1e2,1e3,1e4,1e5]
+        #ticks4 = [-1,-0.7,-0.4,-0.1,0.1,0.4,0.7,1]
         ticks4_label = [str(o) for o in ticks4]
         flag2=ticks
         
-        #plt.contourf(x,y,data,ticks4,norm=colors.SymLogNorm(linthresh = 1e1,linscale = 1e0,vmin=-1e3,vmax=1e3),cmap='RdYlBu_r', format = '%.0e')
-        #plt.contourf(x,y,data,ticks3,norm=colors.LogNorm(vmin=1e-5,vmax=8e4),cmap='RdYlBu_r', format = '%.0e')
-        plt.contourf(x,y,data,ticks4,norm=colors.SymLogNorm(linthresh = 1e1,linscale = 1e0,vmin=-5e3,vmax=5e3),cmap='RdYlBu_r', format = '%.0e')
-        #plt.contourf(x,y,data,ticks3,norm=colors.LogNorm(vmin=pow(10,1),vmax=pow(10,6)),cmap=plt.cm.jet,resolution='c', format = '%.0e')
+        #plt.contourf(x,y,data,ticks4,norm=colors.SymLogNorm(linthresh = 1e1,linscale = 1e0,vmin=-1e7,vmax=1e7),cmap='RdYlBu_r', format = '%.0e')
+        #plt.contourf(x,y,data,ticks4,vmin = np.min(ticks4), vmax = np.max(ticks4) ,cmap='RdYlBu_r', format = '%.0e')
+        plt.contourf(x,y,data,ticks4,norm=colors.LogNorm(vmin=0.1,vmax=1e5),cmap='RdYlBu_r', format = '%.0e')
+        #plt.contourf(x,y,data,ticks4,norm=colors.SymLogNorm(linthresh = 1e1,linscale = 1e0,vmin=-1,vmax=1),cmap='RdYlBu_r', format = '%.0e')
+       # plt.contourf(x,y,data,ticks4,norm=colors.SymLogNorm(linthresh = 1e1,linscale = 1e0,vmin=-1e200,vmax=1e200),cmap='RdYlBu_r', format = '%.0e')
         #plt.clim((pow(10,3),pow(10,7)))
         plt.plot(lat1,trop_height.data,'k--')
         mn=0.1
@@ -246,6 +290,9 @@ for i in flag:
         print('\nThe maximum value of particle number is      = ', np.max(data))
         print('\nThe minimum value of particle number is      = ', np.min(data))
         print('\nThe mean value of particle number is         = ', np.mean(data))
+        print('\nThe max value at surface is                  = ', np.max(data[0,:]))
+        print('\nThe min value of surface is                  = ', np.min(data[0,:]))
+        print('\nThe mean value of surface is                 = ', np.mean(data[0,:]))
         
         #plt.contourf(x,y,data,cmap=,resolution='c')
         formatter = LogFormatter(10, labelOnlyBase=False)
@@ -263,12 +310,14 @@ for i in flag:
         plt.xlabel('latitude')
         plt.ylabel('altitude (km)')
         #plt.title('Total Particle number concentration (cm'+u'\u207B\u00B3'+')')
+        plt.title('oh_calcnucrate/oh_chem')
+        #plt.title('(sec_org) concentration molecules/cc')
+        #plt.title('(HOM) concentration molecules/cc')
         #plt.title('N_Total change (sulphurless+purebiogenic) - (sulphurless)(cm'+u'\u207B\u00B3'+')' )
-        plt.title('N_Total change (sulphurless + biogen) - (sulphurless)(cm'+u'\u207B\u00B3'+') u-cb602 - u-ca440' )
-        plt.savefig('sulphurless_purebiogenic.png',dpi = 500)
+        plt.savefig('ntot_vertical_profile.eps',dpi = 500)
         plt.show()
         
-        print(new1)       
+        #print(new1)       
         
 
 
